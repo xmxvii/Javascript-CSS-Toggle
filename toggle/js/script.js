2ae7e5d6ce7f4ea2a2150c99baebf1656704b6d3
Cristian <35613226+cristianvii@users.noreply.github.com>
2020-12-24 14:12:36 -0500

    Delete script.js
    duplicates

--- toggle/js/script.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('change', () => {
-    // change the theme of the website
-    document.body.classList.toggle('dark');
-});
